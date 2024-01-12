d204d941aae44c2bdd77f4a1b8ee9906d81748e1
intouch.opec <intouch.opec@gmail.com>
2024-01-12 00:30:01 -0500

    fix: fix condition and rename value.

--- number_guess.sh
+++ number_guess.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 MIN_GUESS_NUMBER=1
 MAX_GUESS_NUMBER=1000
-GUESS_NUMBER=$((RANDOM % ($MAX_GUESS_NUMBER - $MIN_GUESS_NUMBER + 1) + $MIN_GUESS_NUMBER))
+SECRET_NUMBER=$((RANDOM % ($MAX_GUESS_NUMBER - $MIN_GUESS_NUMBER + 1) + $MIN_GUESS_NUMBER))
 echo -e "Enter your username: "
 read USERNAME_INPUT 
 PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
 USERS=$($PSQL "SELECT user_id, username FROM users WHERE username = '$USERNAME_INPUT'; ")
 
-function VAlIDATE_SECRET_NUMBER() {
-  read SECRET_NUMBER
-  if  [[ ! "$SECRET_NUMBER" =~ ^[0-9]+$ ]]; 
+function GUESSING_GAME() {
+  read GUESS_NUMBER_INPUT
+  if  [[ ! "$GUESS_NUMBER_INPUT" =~ ^[0-9]+$ ]]; 
   then
     echo -e "That is not an integer, guess again:"
-    VAlIDATE_SECRET_NUMBER 
+    GUESSING_GAME 
   fi
-  if [ $SECRET_NUMBER -lt $MIN_GUESS_NUMBER ];
+  if [ $SECRET_NUMBER -lt $GUESS_NUMBER_INPUT ];
   then
-    echo -e "It's lower than that, guess again:" 
-    VAlIDATE_SECRET_NUMBER 
-  fi
-  if [ $SECRET_NUMBER -gt $MAX_GUESS_NUMBER ];
-  then 
     echo "It's higher than that, guess again:"
-    VAlIDATE_SECRET_NUMBER
+    GUESSING_GAME 
   fi
-  return $SECRET_NUMBER
+  if [ $SECRET_NUMBER -gt $GUESS_NUMBER_INPUT ];
+  then 
+    echo -e "It's lower than that, guess again:" 
+    GUESSING_GAME
+  fi
+  return $GUESS_NUMBER_INPUT
 }
 
 if [[ -z $USERS ]]
 then 
   echo -e "Welcome, $USERNAME_INPUT! It looks like this is your first time here."
-  echo "Guess the secret number between 1 and 1000:"
-  VAlIDATE_SECRET_NUMBER 
-  echo $SECRET_NUMBER
 else 
   echo -e "Welcom back, $USERNAME_INPUT! You have play $NUMBER_OF_GUESS games, and your best game took $BEST_GAME guesses"
 fi
+echo "Guess the secret number between 1 and 1000:"
+GUESSING_GAME 

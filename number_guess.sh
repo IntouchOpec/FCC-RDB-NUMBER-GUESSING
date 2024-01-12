2558e90ee5391219dc6418066892f60eaf5addf5
intouch.opec <intouch.opec@gmail.com>
2024-01-12 02:44:46 -0500

    feat: add function update statistics

--- number_guess.sh
+++ number_guess.sh
@@ -5,7 +5,9 @@ SECRET_NUMBER=$((RANDOM % ($MAX_GUESS_NUMBER - $MIN_GUESS_NUMBER + 1) + $MIN_GUE
 echo -e "Enter your username: "
 read USERNAME_INPUT 
 PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
-USERS=$($PSQL "SELECT user_id, username FROM users WHERE username = '$USERNAME_INPUT'; ")
+USERS=$($PSQL "SELECT user_id, username, toltal_number_of_games, fewest_number_of_guesses FROM users WHERE username = '$USERNAME_INPUT'; ")
+
+COUNT_GUESS=$(1)
 
 function GUESSING_GAME() {
   read GUESS_NUMBER_INPUT
@@ -24,14 +26,29 @@ function GUESSING_GAME() {
     echo -e "It's lower than that, guess again:" 
     GUESSING_GAME
   fi
+  COUNT_GUESS=$(($COUNT_GUESS+1))
   return $GUESS_NUMBER_INPUT
 }
 
 if [[ -z $USERS ]]
 then 
   echo -e "Welcome, $USERNAME_INPUT! It looks like this is your first time here."
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users (username, toltal_number_of_games, fewest_number_of_guesses) VALUES ('$USERNAME_INPUT',0,0);")
 else 
-  echo -e "Welcom back, $USERNAME_INPUT! You have play $NUMBER_OF_GUESS games, and your best game took $BEST_GAME guesses"
+  echo "$USERS" | while read USER_ID BAR USERNAME BAR TOLTAL_NUMBER_OF_GAMES BAR FEWEST_NUMBER_OF_GUESSES
+    do
+      echo -e "Welcome back, $USERNAME_INPUT! You have played $TOLTAL_NUMBER_OF_GAMES games, and your best game took $FEWEST_NUMBER_OF_GUESSES guesses."
+    done
 fi
+
 echo "Guess the secret number between 1 and 1000:"
 GUESSING_GAME 
+if [ $SECRET_NUMBER -eq $GUESS_NUMBER_INPUT ];
+then
+  if [[ $FEWEST_NUMBER_OF_GUESSES -lt $COUNT_GUESS ]]
+  then
+      FEWEST_NUMBER_OF_GUESSES=$(($COUNT_GUESS + 1))
+  fi 
+  TOLTAL_NUMBER_OF_GAMES=$(($TOLTAL_NUMBER_OF_GAMES + 1))
+  UPDATED_USER_RESULT=$($PSQL "UPDATE users SET toltal_number_of_games = $TOLTAL_NUMBER_OF_GAMES, fewest_number_of_guesses = $FEWEST_NUMBER_OF_GUESSES WHERE username = '$USERNAME_INPUT';")
+fi

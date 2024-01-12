6312a7fde625ff537b07884ed73c2e405b3d4852
intouch.opec <intouch.opec@gmail.com>
2024-01-11 22:15:25 -0500

    feat: add connecntion database.

--- number_guess.sh
+++ number_guess.sh
@@ -2,7 +2,6 @@
 MIN_GUESS_NUMBER=1
 MAX_GUESS_NUMBER=1000
 GUESS_NUMBER=$((RANDOM % ($MAX_GUESS_NUMBER - $MIN_GUESS_NUMBER + 1) + $MIN_GUESS_NUMBER))
-echo $GUESS_NUMBER 
 echo -e "Enter your username: "
 read USERNAME_INPUT 
-echo $USERNAME_INPUT
+PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"

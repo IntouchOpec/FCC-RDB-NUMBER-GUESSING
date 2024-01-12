4d5e87037ad2422201e2437671f7f1f607b1c0a9
intouch.opec <intouch.opec@gmail.com>
2024-01-11 23:57:24 -0500

    fix: rename table

--- number_guess.sh
+++ number_guess.sh
@@ -5,32 +5,33 @@ GUESS_NUMBER=$((RANDOM % ($MAX_GUESS_NUMBER - $MIN_GUESS_NUMBER + 1) + $MIN_GUES
 echo -e "Enter your username: "
 read USERNAME_INPUT 
 PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
-
-GUESS=$($PSQL "SELECT guess_id, username FROM guesses WHERE username = '$USERNAME_INPUT'; ")
+USERS=$($PSQL "SELECT user_id, username FROM users WHERE username = '$USERNAME_INPUT'; ")
 
 function VAlIDATE_SECRET_NUMBER() {
-  echo $1
   read SECRET_NUMBER
   if  [[ ! "$SECRET_NUMBER" =~ ^[0-9]+$ ]]; 
   then
-    VAlIDATE_SECRET_NUMBER "That is not an integer, guess again:"
+    echo -e "That is not an integer, guess again:"
+    VAlIDATE_SECRET_NUMBER 
   fi
   if [ $SECRET_NUMBER -lt $MIN_GUESS_NUMBER ];
   then
-    VAlIDATE_SECRET_NUMBER "It's lower than that, guess again:" 
+    echo -e "It's lower than that, guess again:" 
+    VAlIDATE_SECRET_NUMBER 
   fi
   if [ $SECRET_NUMBER -gt $MAX_GUESS_NUMBER ];
   then 
-    VAlIDATE_SECRET_NUMBER "It's higher than that, guess again:"
+    echo "It's higher than that, guess again:"
+    VAlIDATE_SECRET_NUMBER
   fi
   return $SECRET_NUMBER
 }
 
-
-if [[ -z $GUESS ]]
+if [[ -z $USERS ]]
 then 
   echo -e "Welcome, $USERNAME_INPUT! It looks like this is your first time here."
-  VAlIDATE_SECRET_NUMBER "Guess the secret number between 1 and 1000:"
+  echo "Guess the secret number between 1 and 1000:"
+  VAlIDATE_SECRET_NUMBER 
   echo $SECRET_NUMBER
 else 
   echo -e "Welcom back, $USERNAME_INPUT! You have play $NUMBER_OF_GUESS games, and your best game took $BEST_GAME guesses"

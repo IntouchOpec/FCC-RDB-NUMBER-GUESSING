86b84c274de128d2fb79b89bc56d9505dbbc0522
intouch.opec <intouch.opec@gmail.com>
2024-01-11 22:34:01 -0500

    feat: add message welcome old guess and new guess.

--- number_guess.sh
+++ number_guess.sh
@@ -5,3 +5,13 @@ GUESS_NUMBER=$((RANDOM % ($MAX_GUESS_NUMBER - $MIN_GUESS_NUMBER + 1) + $MIN_GUES
 echo -e "Enter your username: "
 read USERNAME_INPUT 
 PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
+
+GUESS=$($PSQL "SELECT guess_id, username FROM guesses WHERE username = '$USERNAME_INPUT'; ")
+
+if [[ -z $GUESS ]]
+then 
+  echo -e "Welcome, $USERNAME_INPUT! It looks like this is your first time here."
+  echo -e "Guesss the secret number between 1 and 1000:"
+else 
+  echo -e "Welcom back, $USERNAME_INPUT! You have play $NUMBER_OF_GUESS games, and your best game took $BEST_GAME guesses"
+fi
